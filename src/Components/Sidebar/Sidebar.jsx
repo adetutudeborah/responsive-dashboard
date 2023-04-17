d56d67b2709e15f6ee1f4774cb4e1fddba6fc0ab
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-04-17 11:07:03 +0100

    added the sidebar section

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,69 @@
 import React from 'react'
 import './Sidebar.css'
+import image from "../../assets/image";
 
 function Sidebar() {
 
   return (
-    <div className='py-9 px-8'>
-        <div>Sidebar</div>
+    <div className='py-9 px-7 flex flex-col justify-between'>
+
+        <div className='mb-auto'>
+
+            <button className='bg-[#45269c] text-white py-3 px-8 rounded-lg text-center inline'>
+              <img src={image.lightning} alt='lightning-icon' className='inline pr-2 text-sm'/>
+              Welcome Keerthi
+            </button>
+
+          <div className='py-10'>
+                <div className='pb-6'>
+                  <img src={image.dashboard} alt='dashboard-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Dashboard</h4>
+                </div>
+
+                <div className='pb-6'>
+                  <img src={image.settings} alt='settings-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Settings</h4>
+                </div>
+
+                <div className='pb-6'>
+                  <img src={image.activities} alt='activities-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Activities</h4>
+                </div>
+
+                <div className='pb-6'>
+                  <img src={image.users} alt='users-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Users</h4>
+                </div>
+
+                <div className='pb-6'>
+                  <img src={image.addeduser} alt='added-user-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Added user</h4>
+                </div>
+
+                <div className='pb-6'>
+                  <img src={image.affiliate} alt='affiliate-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Affiliate</h4>
+                </div>
+
+
+                <div className='pb-6 pt-6 opacity-50 border-t-2 border-white'>
+                  <img src={image.profile} alt='profile-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Profile</h4>
+                </div>
+
+                <div className='pb-6 opacity-50'>
+                  <img src={image.logout} alt='logout-icon' className='inline pr-3 pb-1'/>
+                  <h4 className='text-white inline text-sm'>Logout</h4>
+                </div>
+          </div>
+        </div>
+
+        <div className='order-last justify-end'>
+          <button className='bg-[#703EFE] text-white py-3 px-9 rounded-lg text-center inline uppercase font-normal tracking-wide'>
+            <img src={image.plus} alt='plus-icon' className='inline pr-2 pb-1 text-sm text-white'/>
+             New Project
+          </button>
+        </div>
     </div>
   )
 }
--- src/assets/image.js
+++ src/assets/image.js
@@ -1,11 +1,29 @@
-// import logo from './header/logo.png';
+import lightning from './sidebar-icons/lightning-icon.png';
+import dashboard from './sidebar-icons/dashboard-icon.png';
+import settings from './sidebar-icons/settings-icon.png';
+import activities from './sidebar-icons/activities-icon.png';
+import users from './sidebar-icons/users-icon.png';
+import addeduser from './sidebar-icons/added-user-icon.png';
+import affiliate from './sidebar-icons/affiliate-icon.png';
+import profile from './sidebar-icons/profile-icon.png';
+import logout from './sidebar-icons/logout-icon.png';
+import plus from './sidebar-icons/plus-icon.png';
 
 
-// const images = {
-//   logo,
-// };
+const images = {
+  lightning,
+  dashboard,
+  settings,
+  activities,
+  users,
+  addeduser,
+  affiliate,
+  profile,
+  logout,
+  plus,
+};
 
-// export default images;
+export default images;
 
 // import images from "../../assets/image";
 // To import the images in other folders
