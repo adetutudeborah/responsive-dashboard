a64339e2a5d192838691a8ec341790145e4d89c7
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-04-18 18:48:38 +0100

    added the design sections

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -20,7 +20,7 @@ function Main() {
 
                 {/* First task */}
 
-                <div className='bg-white mt-4 p-4'>
+                <div className='bg-white mt-4 p-4 rounded-xl'>
 
                     <div className='bg-[#FBC7E0] w-[100%] h-[168px] flex items-center justify-center'>
                          <img src={image.designone} alt="design-one"/>
@@ -66,7 +66,7 @@ function Main() {
                 
                 {/* Second task */}
 
-                <div className='bg-white mt-4 p-4'>
+                <div className='bg-white mt-4 p-4 rounded-xl'>
 
                     <div className='flex flex-row justify-between pt-4'>
 
@@ -108,7 +108,7 @@ function Main() {
                
                  {/* Add task */}
                  
-                <div className='bg-[#f2f4f5] flex flex-row justify-center mt-8 mb-4 py-3 px-3 rounded-xl border-dashed border-2 border-[#D5D5D5]'>
+                <div className='bg-[#f2f4f5] flex flex-row justify-center mt-8 py-3 px-3 rounded-xl border-dashed border-2 border-[#D5D5D5]'>
                     <img src={image.addtask} alt='add-task-icon' className='inline pr-3  h-[18px] mt-1'/>
                     <p className='text-[#7D8088] font-medium text-base inline'>Add Task</p>
                 </div>
@@ -120,8 +120,62 @@ function Main() {
             <div className='col-start-2'>
 
                 <div className='bg-[#F4F6F8] flex flex-row justify-between py-2 px-4 rounded-lg'>
-                    <p className='text-[#858A93] font-medium text-base pt-2'>Inprogress</p>
+                    <p className='text-[#858A93] font-medium text-base pt-2'>In progress</p>
                     <p className='bg-black py-2 px-4 rounded text-white text-lg'>1</p>
+                </div>
+
+                 {/* First task */}
+
+                <div className='bg-white mt-4 p-4 rounded-xl'>
+
+                    <div className='bg-[#D9D3CE] w-[100%] h-[168px] flex items-center justify-center'>
+                         <img src={image.designtwo} alt="design-two"/>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+
+                        <div className=''>                   
+                          <h3 className='text-black text-base font-semibold pt-2'>Highfidelity Design</h3>
+                          <p className='text-[#B8B9BD] text-sm font-normal pt-1'>Make clear design and color</p>
+                       </div>
+
+                       <div className='mt-2'>
+                          <img src={image.threedot} alt="three-dot" className=''/>
+                       </div>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+                        <div className=''>
+                          <img src={image.progress} alt='progress-icon' className='inline pr-2 pb-1'/>
+                          <h4 className='text-[#B8B9BD] inline text-sm'>Progress</h4>
+                       </div>
+
+                       <div>
+                         <p className='text-black font-medium text-sm mt-1'>2/10</p>
+                       </div>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+                       
+                        <div className=''>  
+                          <span className='text-[#B8B9BD] inline text-sm mr-4'><img src={image.comment} alt='comment-icon' className='inline mr-1'/>7</span>
+                          <span className='text-[#B8B9BD] inline text-sm mr-4'><img src={image.link} alt='link-icon' className='inline mr-1'/>2</span>
+                       </div>
+
+                       <div>
+                          <img src={image.mainprofilestack} alt='main-profile-stack'/>
+                       </div>
+
+                    </div>
+ 
+                </div>
+              
+               
+                 {/* Add task */}
+                 
+                <div className='bg-[#f2f4f5] flex flex-row justify-center mt-6 py-3 px-3 rounded-xl border-dashed border-2 border-[#D5D5D5]'>
+                    <img src={image.addtask} alt='add-task-icon' className='inline pr-3  h-[18px] mt-1'/>
+                    <p className='text-[#7D8088] font-medium text-base inline'>Add Task</p>
                 </div>
 
             </div>
@@ -135,7 +189,105 @@ function Main() {
                     <p className='bg-black py-2 px-4 rounded text-white text-lg'>2</p>
                 </div>
 
+                {/* Second task */}
+
+                <div className='bg-white mt-4 p-4 rounded-xl'>
+
+                    <div className='flex flex-row justify-between pt-4'>
+
+                        <div className=''>                   
+                          <h3 className='text-black text-base font-semibold pt-2'>Usability testing</h3>
+                          <p className='text-[#B8B9BD] text-sm font-normal pt-1'>Make clear design and color</p>
+                       </div>
+
+                       <div className='mt-2'>
+                          <img src={image.threedot} alt="three-dot" className=''/>
+                       </div>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+                        <div className=''>
+                          <img src={image.progress} alt='progress-icon' className='inline pr-2 pb-1'/>
+                          <h4 className='text-[#B8B9BD] inline text-sm'>Progress</h4>
+                       </div>
+
+                       <div>
+                         <p className='text-black font-medium text-sm mt-1'>2/10</p>
+                       </div>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+                       
+                        <div className=''>  
+                          <span className='text-[#B8B9BD] inline text-sm mr-4'><img src={image.comment} alt='comment-icon' className='inline mr-1'/>7</span>
+                          <span className='text-[#B8B9BD] inline text-sm mr-4'><img src={image.link} alt='link-icon' className='inline mr-1'/>2</span>
+                       </div>
+
+                       <div>
+                          <img src={image.mainprofilestack} alt='main-profile-stack'/>
+                       </div>
+
+                    </div>
+ 
+                </div>
+
+                 {/* First task */}
+
+                <div className='bg-white mt-4 p-4 rounded-xl'>
+
+                    <div className='bg-[#597474] w-[100%] h-[168px] flex items-center justify-center'>
+                         <img src={image.designthree} alt="design-three"/>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+
+                        <div className=''>                   
+                          <h3 className='text-black text-base font-semibold pt-2'>Highfidelity Design</h3>
+                          <p className='text-[#B8B9BD] text-sm font-normal pt-1'>Make clear design and color</p>
+                       </div>
+
+                       <div className='mt-2'>
+                          <img src={image.threedot} alt="three-dot" className=''/>
+                       </div>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+                        <div className=''>
+                          <img src={image.progress} alt='progress-icon' className='inline pr-2 pb-1'/>
+                          <h4 className='text-[#B8B9BD] inline text-sm'>Progress</h4>
+                       </div>
+
+                       <div>
+                         <p className='text-black font-medium text-sm mt-1'>2/10</p>
+                       </div>
+                    </div>
+
+                    <div className='flex flex-row justify-between pt-4'>
+                       
+                        <div className=''>  
+                          <span className='text-[#B8B9BD] inline text-sm mr-4'><img src={image.comment} alt='comment-icon' className='inline mr-1'/>7</span>
+                          <span className='text-[#B8B9BD] inline text-sm mr-4'><img src={image.link} alt='link-icon' className='inline mr-1'/>2</span>
+                       </div>
+
+                       <div>
+                          <img src={image.mainprofilestack} alt='main-profile-stack'/>
+                       </div>
+
+                    </div>
+ 
+                </div>
+              
+               
+                 {/* Add task */}
+                 
+                <div className='bg-[#f2f4f5] flex flex-row justify-center mt-8 py-3 px-3 rounded-xl border-dashed border-2 border-[#D5D5D5]'>
+                    <img src={image.addtask} alt='add-task-icon' className='inline pr-3  h-[18px] mt-1'/>
+                    <p className='text-[#7D8088] font-medium text-base inline'>Add Task</p>
+                </div>
+
+
             </div>
+            
         
         </div>
 
